b0f993ecb7143307ceaa23c57db21d0c29ce9b09
Lenni <lennart@familie-richter.de>
2022-12-11 21:23:10 -0800

    Finished Parts 2 and 3, reimplimented Part 2 using pickle for cleaner code

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
+import pickle
 class DictionaryFileManager():
     def __init__(self, filename:str) -> None:
         self.filename = filename
     
     def write_dictionary_to_file(self, passed_in_dictionary:dict) -> None:
-        with open('dictionary.txt', 'w') as f:
-            for i in passed_in_dictionary:
-                f.write(f"{i}, {passed_in_dictionary[i]}")
+        with open('dictionary.txt', 'wb') as f:
+            pickle.dump(passed_in_dictionary,f)
 
     def read_dictionary_from_file(self) -> dict:
         dictionary_from_file = {}
         try:
-            with open('dictionary.txt', 'r') as f:
-                for line in f:
-                    name_number_list_from_file = line.split(',')
-                    dictionary_from_file[name_number_list_from_file[0]] = name_number_list_from_file[1]
+            with open('dictionary.txt', 'rb') as f:
+                dictionary_from_file = pickle.load(f)
         except FileNotFoundError:
             return {}
         return dictionary_from_file
@@ -31,5 +29,6 @@ def main():
     name_to_lookup = input("Enter name to look up: ").capitalize()
     if name_to_lookup.capitalize() in building_numbers:
         print(building_numbers[name_to_lookup])
+    manager.write_dictionary_to_file(building_numbers)
 
 main()
